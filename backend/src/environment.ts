08866a82c6e85632142ce825bd55ead37a6f9028
Jasmine Dillard <jdilldev@gmail.com>
2022-06-11 16:30:31 -0400

    realmdb seems great but very confused on how to setup with node and apollo and graphql

--- backend/src/environment.ts
+++ backend/src/environment.ts
@@ -1,9 +1,11 @@
 type Environment = {
 	STYTCH_PROJECT_ID: string;
 	STYTCH_SECRET: string;
+	REALM_APP_ID: string;
 };
 
 export default {
 	STYTCH_PROJECT_ID: String(process.env.STYTCH_PROJECT_ID),
 	STYTCH_SECRET: String(process.env.STYTCH_SECRET),
-};
+	REALM_APP_ID: String(process.env.REALM_APP_ID),
+} as Environment;
